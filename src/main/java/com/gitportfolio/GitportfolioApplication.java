5a6d043e2b2dc3a3da901b7a87bc9a2a860f89fd
youns <youns@Youns>
2025-01-15 05:00:26 +0900

    first commit

---
+++ src/main/java/com/gitportfolio/GitportfolioApplication.java
@@ -0,0 +1,13 @@
+package com.gitportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitportfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitportfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitportfolio/GitportfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitportfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
