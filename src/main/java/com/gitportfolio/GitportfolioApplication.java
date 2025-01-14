ca42f4460b6adfad26eff0384368bf0c39ebd84e
youns <youns@Youns>
2025-01-15 05:07:35 +0900

    test commit

--- src/main/java/com/gitportfolio/GitportfolioApplication.java
+++ src/main/java/com/gitportfolio/GitportfolioApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitportfolioApplication {
 
 	public static void main(String[] args) {
+		//test
 		SpringApplication.run(GitportfolioApplication.class, args);
 	}
 
